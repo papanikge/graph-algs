4937a2847cf35ba607c35c77a80d01badf7ff978
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-05-13 14:40:58 +0300

    implement cost edge array

--- kruskal_mst/leda.cc
+++ kruskal_mst/leda.cc
@@ -8,42 +8,45 @@
 #include <cstdlib>
 #include <cmath>
 #include <cstring>
+#include <ctime>
 #include <LEDA/graph/graph.h>
 #include <LEDA/graph/graph_gen.h>
-#include <LEDA/graph/node_array.h>
-#include <LEDA/graph/basic_graph_alg.h>
 #include <LEDA/graph/min_span.h>
 #include <LEDA/system/timer.h>
 
 /*
- * Wrapper around random graph generation. Calculates number-of-nodes to be
- * the theoretical maximum so the graph would be fully connected (aka complete).
- * TODO: eventually we need to be able to generate boost *and* LEDA graphs,
- * so we may need to make this a class of some sort??
+ * Wrapper around undirected random graph generation with random edge weights.
+ * Calculates number-of-nodes to be the theoretical maximum so the graph would
+ * be fully connected (aka complete).
+ * TODO: eventually we need to be able to generate boost *and* LEDA graphs, so we
+ * may need to make this a class of some sort (?)
  */
-void connected_random_generator(leda::graph& G, int number_of_nodes)
+void connected_random_generator(leda::graph& G, int number_of_nodes, leda::edge_array<int>& cost)
 {
-    int number_of_edges = 2*number_of_nodes*log(number_of_nodes);
+    leda::edge e;
+    int number_of_edges = 2 * number_of_nodes * log10(number_of_nodes);
 
     leda::random_simple_undirected_graph(G, number_of_nodes, number_of_edges);
-    std::cout << "Graph generated. Nodes: " << number_of_nodes << std::endl;
+    // assigning random weights
+    cost.init(G, 0);
+    srand(time(NULL));
+    forall_edges(e, G) {
+        cost[e] = rand() % 10000 + 1;
+    }
 
-    ///// TODO: WHERE ???>>
-    // leda::node_array<int> compnum(G, 0);
+    std::cout << "Graph generated. Nodes: " << number_of_nodes << std::endl;
     return;
 }
 
 /*
  * Main function to run all the MST versions and benchmark their time
  */
-void benchmark_implementations(leda::graph& G)
+void benchmark_implementations(leda::graph& G, leda::edge_array<int>& cost)
 {
     float T;
-    leda::node_array<int> sccnum; // TODO: DEFINITELY useless. we need to init it somewhere else and with diff stuff
 
-    sccnum.init(G, 0);
     T = leda::used_time();
-    leda::MIN_SPANNING_TREE(G, *sccnum);
+    leda::MIN_SPANNING_TREE(G, cost);
     std::cout << "\t\tLEDA MST calculation time: " << leda::used_time(T) << std::endl;
 }
 
@@ -51,20 +54,22 @@ int main(int argc, char **argv)
 {
     int i, n;
     leda::graph G;
+    leda::edge e; //// TODO: REMOVE THIS
+    leda::edge_array<int> cost;
     int N[] = { 10000, 40000, 70000 };
 
     if (argc > 2 && !strcmp(argv[1], "-n")) {
         n = atoi(argv[2]);
-        connected_random_generator(G, n);
-        benchmark_implementations(G);
+        connected_random_generator(G, n, cost);
+        benchmark_implementations(G, cost);
     } else {
-        std::cout << ">>> Minimum Spanning Tree Benchmarking <<<\n";
+        std::cout << "\n>>> Minimum Spanning Tree Benchmarking <<<\n";
         std::cout << "Give -n <number of nodes> if you want custom nodes\n";
         std::cout << "Moving on with the default number of nodes...\n\n";
         for (i = 0; i < 3; i++) {
             std::cout << ">>> Random graphs...";
-            connected_random_generator(G, N[i]);
-            benchmark_implementations(G);
+            connected_random_generator(G, N[i], cost);
+            benchmark_implementations(G, cost);
 
             // clean up
             G.clear();
@@ -78,7 +83,15 @@ int main(int argc, char **argv)
             // this is a hack, LEDA cannot produce an undirected grid
             // graph, so we make them bidirectional.
             G.make_bidirected();
-            benchmark_implementations(G);
+
+            // assigning random weights
+            // TODO: these need to get on their own function
+            cost.init(G, 0);
+            srand(time(NULL));
+            forall_edges(e, G) {
+                cost[e] = rand() % 10000 + 1;
+            }
+            benchmark_implementations(G, cost);
 
             // clean up
             G.clear();
