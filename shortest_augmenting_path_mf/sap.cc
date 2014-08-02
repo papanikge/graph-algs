cbd63b1fde84a6becbc17bd90ca6343cb2b52cc2
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-08-02 12:20:37 +0300

    add some structure for the bfs

--- shortest_augmenting_path_mf/sap.cc
+++ shortest_augmenting_path_mf/sap.cc
@@ -8,7 +8,24 @@
 #include "boost-types.h"
 #include <boost/graph/breadth_first_search.hpp>
 
+/*
+ * subclassing the default visitor for our own needs
+ */
+class MyVisitor : public boost::default_dfs_visitor
+{
+public:
+    void discover_vertex(BoostVertex v, const BoostGraph& g) const
+    {
+        // TODO something when discovering the node
+        return;
+    }
+};
+
+/*
+ * this file's core function
+ */
 int shortest_aug_path(BoostGraph& BG, std::vector<BoostEdge> ret_flow)
 {
-    // TODO
+    MyVisitor vis
+    breadth_first_search(BG, boost::visitor(vis)); // add second argument the intial core if desired
 }
