2c2975cff156be1c482c0ab3870206d3a0f0b2ef
George 'papanikge' Papanikolaou <g3orge.app@gmail.com>
2014-09-01 23:09:55 +0300

    implement trace aug path function, fix some index errors

--- shortest_augmenting_path_mf/sap.cc
+++ shortest_augmenting_path_mf/sap.cc
@@ -42,29 +42,57 @@ static inline int find_min_out_edges(const BoostVertex& initial,
 }
 
 /*
+ * Tracing the path back to the source and finding the minimux residue capacity
+ */
+static int min_res_on_path(BoostGraph& BG, VerticesSizeType *parent, int f)
+{
+    int cap;
+    int delta = 1000;  /* something big so we can find smaller values */
+    BoostVertex u, v;
+
+    while (parent[f] != NULL) {
+        /* getting the capacity */
+        u = boost::vertex(f, BG);
+        v = boost::vertex(parent[f], BG);
+        cap = BG[boost::edge(u, v, BG).first];
+        /* trying to find min */
+        if (delta > cap)
+            delta = cap;
+        /* next... */
+        f = parent[f];
+    }
+    return delta;
+}
+
+/*
  * this file's core function
  */
-int shortest_aug_path(BoostGraph& BG, BoostVertex& s, BoostVertex& t)
+int shortest_aug_path(BoostGraph& BG, BoostVertex& source, BoostVertex& target)
 {
-    int i, j;
+    int i, j, s, t, delta;
     unsigned int n = boost::num_vertices(BG);
     unsigned int m = boost::num_edges(BG);
     BoostOutEdgeIt current, next;
     std::vector<BoostVertex> avail;
-    /* Vertices index map */
+    /* We use an "index map" in order to easily assign IDs to vertices, and be able
+     * to use regular arrays. Boost visitors use regular arrays and they are simpler
+     * than using Propery Maps */
     IndexMap index = boost::get(boost::vertex_index, BG);
     /* We'll just use a regular array to store the distances for the sake of simplicity. */
     VerticesSizeType distances[n];
     std::fill_n(distances, n, 0);
     /* ...and a similar one for the parent nodes */
     VerticesSizeType parent[n];
-    std::fill_n(parent, n, NULL);
+    std::fill_n(parent, n, NULL); // TODO: maybe null is not allowed. in that case choose an unchosen id
 
     /* Getting the distance labels by reversed BFS. Creating the visitor inline. */
     boost::breadth_first_search(boost::make_reverse_graph(BG), boost::vertex(t, BG),
                boost::visitor(boost::make_bfs_visitor(boost::record_distances(&distances[0],
                                                                               boost::on_tree_edge())))); 
 
+    /* getting the index, since we're working we them now, and starting main loop... */
+    s = index[source];
+    t = index[target];
     i = s;
     while (distances[s] < n) {
         /* Iterating through all out going edges of current node */
@@ -76,14 +104,16 @@ int shortest_aug_path(BoostGraph& BG, BoostVertex& s, BoostVertex& t)
 
         if (avail.size() != 0) {
             /* Get the requirements */
-            /* TODO: need to remove that index-BS */
-            j = index[avail[0]];
+            j = index[avail[0]]; // TODO: (maybe) we need a way to go to the second
             /* ADVANCE operation */
             parent[j] = i;
             i = j;
             if (i == t) {
-                /* We're there. AUGMENT operation. */
-                // TODO
+                /* We're there. AUGMENT operation.
+                 * We need to find the minimum residual capacity and use it to augment */
+                // TODO: we need to augment and edit the graph
+                delta = min_res_on_path(BG, parent, i);
+                /* Let's go again */
                 i = s;
             }
         } else {
